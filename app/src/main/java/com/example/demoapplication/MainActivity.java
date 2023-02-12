c0a2c519ba3f0ec8d3cbf66eefb7377e0b856947
Prashant Purmah <prashant.purmah@sdworx.com>
2023-02-12 10:07:54 +0100

    lecture 14 implicit intent, locale

--- app/src/main/java/com/example/demoapplication/MainActivity.java
+++ app/src/main/java/com/example/demoapplication/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     TextView counterTextView;
     Button incrementButton;
@@ -35,7 +38,44 @@ public class MainActivity extends AppCompatActivity {
 
         navigateButton.setOnClickListener(view -> {
             Intent navigateIntent = new Intent(MainActivity.this, SecondActivity.class);
+            navigateIntent.putExtra("counter", counter);
             startActivity(navigateIntent);
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "destroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "pause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "resume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "restart");
     }
 }
--- app/src/main/java/com/example/demoapplication/SecondActivity.java
+++ app/src/main/java/com/example/demoapplication/SecondActivity.java
@@ -1,34 +1,111 @@
 package com.example.demoapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
 
     EditText firstNameEditText;
     EditText lastNameEditText;
     Button displayFullNameButton;
     TextView fullNameTextView;
 
+    int counter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Bundle extras = getIntent().getExtras();
+        if(extras != null && extras.containsKey("counter")) {
+            counter = extras.getInt("counter");
+        }
+
         firstNameEditText = findViewById(R.id.editTextFirstName);
         lastNameEditText = findViewById(R.id.editTextLastName);
         displayFullNameButton = findViewById(R.id.buttonDisplayFullName);
         fullNameTextView = findViewById(R.id.textViewFullName);
+        fullNameTextView.setText(Integer.toString(counter));
 
         displayFullNameButton.setOnClickListener(view -> {
             String firstName = firstNameEditText.getText().toString();
             String lastName = lastNameEditText.getText().toString();
             String fullName = firstName + ' '+ lastName;
             fullNameTextView.setText(fullName);
+
+            shareFullName();
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "destroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "pause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "resume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "restart");
+    }
+
+    private void shareFullName() {
+        Toast toast = Toast.makeText(this, "Full name not shared", Toast.LENGTH_SHORT);
+
+        AlertDialog alertDialog =  new AlertDialog.Builder(SecondActivity.this).create();
+        alertDialog.setTitle("Share message");
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Share", (dialogInterface, i) -> {
+            String fullName = fullNameTextView.getText().toString();
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, fullName);
+            shareIntent.setType("text/plain");
+            try {
+                startActivity(shareIntent);
+            } catch (ActivityNotFoundException e) {
+                toast.show();
+            }
+        });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel", (dialogInterface, i) -> {
+            alertDialog.dismiss();
+            toast.show();
+        });
+
+        alertDialog.show();
     }
 }
